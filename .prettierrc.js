c97263dacb7a6a01417f4ff620be120d28e4dc5b
Eugene Kozakov <lointainyyy@gmail.com>
2022-07-01 19:49:57 +0300

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 120,
+  endOfLine: 'auto',
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  htmlFormatWrapAttributes: 'force-aligned',
+}
---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+const path = require('path')
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    sourcemap: false,
+    // assetsDir: 'assets',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @import "@/assets/scss/partials";
+        @import "@/assets/scss/grid";
+        `,
+      },
+    },
+  },
+})
